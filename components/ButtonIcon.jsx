1ffd3ec2b9e2ebbef8b3d90503fbc0aa00265f6a
Christian Petrov <christian.petrov.1@proton.me>
2025-12-11 17:48:56 +0200

    Test

--- components/ButtonIcon.jsx
+++ components/ButtonIcon.jsx
@@ -3,14 +3,18 @@ import Image from "next/image";
 import styles from "/styles/ButtonIcon.module.css";
 
 function ButtonImage({ type, double = false }) {
-    const imagePath = `/img/${type}.png`;
+    const isSvg = type.endsWith(".svg");
+    const baseName = isSvg ? type : `${type}.png`;
+    const imagePath = `/img/${baseName}`;
+
+    const size = double ? { width: 68, height: 32 } : { width: 32, height: 32 };
 
     return (
         <span className={styles.buttonIcon}>
-            {double ? (
-                <Image src={imagePath} alt={type} width={68} height={32} />
+            {isSvg ? (
+                <img src={imagePath} alt={type} width={size.width} height={size.height} />
             ) : (
-                <Image src={imagePath} alt={type} width={32} height={32} />
+                <Image src={imagePath} alt={type} {...size} />
             )}
         </span>
     );
